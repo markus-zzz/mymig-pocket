14e001ffb0be132625ac4b36e31d584398a08118
Markus Lavin <markus.lavin@gmail.com>
2024-12-25 18:34:55 +0100

    Started Copper

--- src/fpga/core/mymig-rtl/mymig.py
+++ src/fpga/core/mymig-rtl/mymig.py
@@ -19,8 +19,16 @@
 # yapf --in-place --recursive --style="{indent_width: 2, column_limit: 120}"
 
 from amaranth import *
+import enum
 
 REG_COLOR00 = 0x180
+
+REG_COP1LCH = 0x080
+REG_COP1LCL = 0x082
+REG_COP2LCH = 0x084
+REG_COP2LCL = 0x086
+REG_COPJMP1 = 0x088
+REG_COPJMP2 = 0x08A
 
 
 ########################################################
@@ -116,13 +124,91 @@ class Copper(Elaboratable):
     # Chip reg write access (egress)
     self.o_chip_reg_addr = Signal(8)
     self.o_chip_reg_data = Signal(16)
+    self.o_chip_reg_wen = Signal()
 
     # Chip reg write access (ingress)
     self.i_chip_reg_addr = Signal(8)
     self.i_chip_reg_data = Signal(16)
+    self.i_chip_reg_wen = Signal()
+
+    # Video
+    self.i_vsync = Signal()
+    self.i_hpos = Signal(9)
+    self.i_vpos = Signal(9)
 
   def elaborate(self, platform):
     m = Module()
+
+    pc = Signal(19)
+    ir = Signal(32)
+    location1 = Signal(19)
+    location2 = Signal(19)
+
+    m.d.comb += self.o_chip_ram_addr.eq(pc)
+
+    class State(enum.Enum):
+      FETCH1 = 0
+      FETCH2 = 1
+      EXECUTE = 2
+    state = Signal(State)
+
+    with m.Switch(state):
+      with m.Case(State.FETCH1):
+        m.d.comb += self.o_chip_ram_req.eq(1)
+        m.d.sync += [
+          ir[0:16].eq(self.i_chip_ram_data),
+          pc.eq(pc + 2),
+          state.eq(State.FETCH2),
+        ]
+      with m.Case(State.FETCH2):
+        m.d.comb += self.o_chip_ram_req.eq(1)
+        m.d.sync += [
+          ir[16:32].eq(self.i_chip_ram_data),
+          pc.eq(pc + 2),
+          state.eq(State.EXECUTE),
+        ]
+      with m.Case(State.EXECUTE):
+        with m.If(ir[0] == 0): # MOVE
+          m.d.comb += [
+            self.o_chip_reg_addr.eq(ir[1:9]),
+            self.o_chip_reg_data.eq(ir[16:32]),
+            self.o_chip_reg_wen.eq(1),
+          ]
+          m.d.sync += state.eq(State.FETCH1)
+        with m.Elif((ir[0] == 1) & (ir[16] == 0)): # WAIT
+          with m.If(((self.i_vpos & ir[24:31]) >= ir[8:16]) & ((self.i_hpos & ir[17:24]) >= ir[1:8])):
+            m.d.sync += state.eq(State.FETCH1)
+        with m.Elif((ir[0] == 1) & (ir[16] == 1)): # SKIP
+          pass
+
+    # Register access
+    with m.If(self.i_chip_reg_wen):
+      with m.Switch(self.i_chip_reg_addr):
+        with m.Case(REG_COP1LCH):
+          m.d.sync += location1[16:19].eq(self.i_chip_reg_data)
+        with m.Case(REG_COP1LCL):
+          m.d.sync += location1[0:16].eq(self.i_chip_reg_data)
+        with m.Case(REG_COP2LCH):
+          m.d.sync += location2[16:19].eq(self.i_chip_reg_data)
+        with m.Case(REG_COP2LCL):
+          m.d.sync += location2[0:16].eq(self.i_chip_reg_data)
+        with m.Case(REG_COPJMP1):
+          m.d.sync += [
+            pc.eq(location1),
+            state.eq(State.FETCH1),
+          ]
+        with m.Case(REG_COPJMP2):
+          m.d.sync += [
+            pc.eq(location2),
+            state.eq(State.FETCH1),
+          ]
+
+    with m.If(self.i_vsync):
+      m.d.sync += [
+        pc.eq(location1),
+        state.eq(State.FETCH1),
+      ]
+
     return m
 
 #
